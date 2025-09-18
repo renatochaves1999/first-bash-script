1ea8ef8b2790f4190dfb2382dfa61176d5ae7e59
Renato Rodrigues Chaves <45311074+renatochaves1999@users.noreply.github.com>
2025-09-18 23:25:24 +0100

    Add files via upload

---
+++ 1script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "What is your name"
+
+read name
+
+echo "Good Morning $name !!"
+
+sleep 1
+
+echo "You're looking good today $name !!"
+
+sleep 1
+
+echo "You have the best beard  I've seen $name !!"
